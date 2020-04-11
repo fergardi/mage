771331bc9903ee7e6781d9e1d0f6d2bbd9864cb9
Fernando Garcia <fernando.garcia@xeridia.com>
2020-04-11 20:18:28 +0200

    Added Firebase.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './home/home.component';
 
+import { environment } from '../environments/environment';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,10 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCSVr_n7Ced40pw2qHEhnr3G09UILvB_zc",
+    authDomain: "mage-c4259.firebaseapp.com",
+    databaseURL: "https://mage-c4259.firebaseio.com",
+    projectId: "mage-c4259",
+    storageBucket: "mage-c4259.appspot.com",
+    messagingSenderId: "210042498621",
+    appId: "1:210042498621:web:c4a21bc796b32d8bf40454"
+  }
 };
