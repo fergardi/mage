14f8431879ead9cfe61d74e7b103970f8f6fe2ca
Fernando Garcia <fernando.garcia@xeridia.com>
2020-04-12 10:49:34 +0200

    Fixed errors with Angular9 and AngularFire6, see https://dev.to/sreekanth_2108/upgrading-to-angularfire-6-with-angular-9-4ce1

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth/auth';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
