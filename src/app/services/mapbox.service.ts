cd4ee5feea1d74962f2d270102cd90febe888459
Fernando Garcia <fernando.garcia@xeridia.com>
2020-04-13 10:38:24 +0200

    Added dynamically generated Popup to marker.

--- src/app/services/mapbox.service.ts
+++ src/app/services/mapbox.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import * as mapboxgl from 'mapbox-gl';
+import { ComponentService } from '../services/component.service';
+import { BuildingComponent } from '../shared/building/building.component';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,7 @@ export class MapboxService {
   mapbox = (mapboxgl as typeof mapboxgl);
   map: mapboxgl.Map;
   
-  constructor() {
+  constructor(private componentService: ComponentService) {
     this.mapbox.accessToken = environment.mapbox.token;
   }
 
@@ -28,14 +30,22 @@ export class MapboxService {
   addMarker(lat: number, lng: number, fly:boolean = false): void {
     var el = document.createElement('div');
     el.className = 'marker';
-    el.style.backgroundImage = 'url(http://localhost:7777/assets/badge.png)';
+    el.style.backgroundImage = 'url("http://localhost:7777/assets/badge.png")';
+    el.style.backgroundSize = '100% 100%';
     el.style.width = '50px';
     el.style.height = '50px';
+
+    let popup = this.componentService.injectComponent(BuildingComponent);
     
     new mapboxgl.Marker(el)
     .setLngLat({ lat: lat, lng: lng })
-    .setPopup(new mapboxgl.Popup({ offset: 25 })
-      .setHTML('<app-building></app-building>'))
+    .setPopup(new mapboxgl.Popup({
+      offset: [-12.5, -30],
+      closeButton: false,
+      closeOnClick: true,
+      closeOnMove: false,
+      maxWidth: 'none',
+    }).setDOMContent(popup))
     .addTo(this.map);
     
     if (fly) {
