cb92ab740cacd77946ec58689e9f01d063848df9
Fernando Garcia <fernando.garcia@xeridia.com>
2020-04-18 12:51:18 +0200

    Linked City component to Firestore collection

--- src/app/kingdom/census/census.component.ts
+++ src/app/kingdom/census/census.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { animate, state, style, transition, trigger } from '@angular/animations';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -8,17 +7,9 @@ import { MatSort } from '@angular/material/sort';
   selector: 'app-census',
   templateUrl: './census.component.html',
   styleUrls: ['./census.component.scss'],
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0' })),
-      state('expanded', style({ height: '*' })),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ],
 })
 export class CensusComponent implements OnInit {
 
-  expanded = null;
   columnsToDisplay = ['img', 'name', 'lands', 'power'];
   data = Array.from({ length: 10 }, _ => {
     return {
--- src/app/kingdom/city/city.component.ts
+++ src/app/kingdom/city/city.component.ts
@@ -1,15 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { switchMap, map } from 'rxjs/operators';
+import { combineLatest } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
+
+interface Building {
+  name: string
+  image: string
+  quantity: number
+  uid: string
+}
 
 @Component({
   selector: 'app-city',
   templateUrl: './city.component.html',
   styleUrls: ['./city.component.scss']
 })
-export class CityComponent implements OnInit {
+export class CityComponent implements AfterViewInit {
 
-  constructor() { }
+  columns = ['name', 'quantity'];
+  data: MatTableDataSource<Building>;
 
-  ngOnInit(): void {
+  constructor(
+    private angularFireAuth: AngularFireAuth,
+    private angularFirestore: AngularFirestore,
+  ) {}
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  applyFilter(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.data.filter = value.trim().toLowerCase();
+  }
+
+  ngAfterViewInit() {
+    this.getUserBuildings().subscribe(data => {
+      this.data = new MatTableDataSource(data);
+      this.data.sort = this.sort;
+      this.data.paginator = this.paginator;
+    })
+  }
+
+  getUserBuildings() {
+    return this.angularFireAuth.authState.pipe(
+      switchMap(user => {
+        if (user) {
+          return combineLatest([
+            this.angularFirestore.collection<Building>(`users/${user.uid}/buildings`).valueChanges(),
+            this.angularFirestore.collection<Building>(`buildings`).valueChanges(),
+          ]).pipe(
+            map(([
+              userBuildings,
+              buildings,
+            ]) => {
+              return userBuildings.map(userBuilding => {
+                return {
+                  ...userBuilding,
+                  ...buildings.find(b => b.uid === userBuilding.uid)
+                }
+              })
+            })
+          )
+        }
+      })
+    )
   }
 
 }
