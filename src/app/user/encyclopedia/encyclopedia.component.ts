9026cdce556db8a44938c811dcab3f298b11b631
Fernando Garcia <fernando.garcia@xeridia.com>
2020-05-02 17:32:10 +0200

    Added white spells

--- src/app/user/encyclopedia/encyclopedia.component.ts
+++ src/app/user/encyclopedia/encyclopedia.component.ts
@@ -5,10 +5,11 @@ import { MatSort } from '@angular/material/sort';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { fadeInOnEnterAnimation } from 'angular-animations';
-import { first } from 'rxjs/operators';
+import { combineLatest, pipe, forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 export enum TomeType {
-  'spell', 'unit', 'hero', 'god', 'building'
+  'spell', 'unit', 'hero', 'god', 'structure'
 }
 
 @Component({
@@ -31,12 +32,28 @@ export class EncyclopediaComponent implements OnInit {
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   ngOnInit() {
-    this.firebaseService.leftJoin('spells', 'factions', 'faction', 'id').pipe(first(), untilDestroyed(this)).subscribe(spells => {
-      console.log(spells)
-      this.data = new MatTableDataSource(spells.map(spell => { return { ...spell, type: TomeType.spell } }));
+    combineLatest([
+      this.firebaseService.leftJoin('spells', 'factions', 'faction', 'id'),
+      this.firebaseService.leftJoin('structures', 'factions', 'faction', 'id'),
+    ]).pipe(
+      untilDestroyed(this),
+      map(([
+        spells,
+        structures,
+      ]) => {
+        console.log(spells, structures)
+        return [
+          ...spells.map(spell => { return { ...spell, type: TomeType.spell } }),
+          ...structures.map(structure => { return { ...structure, type: TomeType.structure } }),
+        ];
+      }
+    ))
+    .subscribe(data => {
+      console.log(data)
+      this.data = new MatTableDataSource(data);
       this.data.paginator = this.paginator;
       this.data.sort = this.sort;
-    })
+    });
   }
 
   applyFilter(event: Event) {
