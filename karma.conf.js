2958ead4d34213921e367815aa60c00047ad2a2b
Fergardi <fergardi@gmail.com>
2021-02-21 18:20:24 +0100

    Added coverage reports to github

--- karma.conf.js
+++ karma.conf.js
@@ -20,8 +20,19 @@ module.exports = function (config) {
       'src/**/*.ts': ['coverage']
     },
     coverageReporter: {
-      type: 'text-summary',
-      dir: 'coverage/'
+      dir: 'coverage/',
+      reporters: [
+        // reporters not supporting the `file` property
+        { type: 'html', subdir: 'html' },
+        { type: 'lcov', subdir: 'lcov' },
+        // reporters supporting the `file` property, use `subdir` to directly
+        // output them in the `dir` directory
+        { type: 'cobertura', subdir: '.', file: 'cobertura.xml' },
+        { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+        { type: 'teamcity', subdir: '.', file: 'teamcity.txt' },
+        { type: 'text', subdir: '.', file: 'text.txt' },
+        { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
+      ]
     },
     reporters: ['progress', 'coverage'],
     port: 9876,
