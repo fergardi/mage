e8deeeb98fcc1ffa15c79f6fdc30eee9b0a74e45
Fernando Garcia <fernando.garcia@xeridia.com>
2021-04-25 14:12:58 +0200

    Added Jest config

---
+++ functions/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+		'^.+\\.tsx?$': 'ts-jest',
+	},
+	// testRegex: 'src(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+	testPathIgnorePatterns: ['lib/', 'node_modules/'],
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+	testEnvironment: 'node',
+	rootDir: '.',
+};
