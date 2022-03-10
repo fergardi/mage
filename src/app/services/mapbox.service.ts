ae4f0e50253f2be674b466a5433b5cdf9d116eb4
Fergardi <fergardi@gmail.com>
2022-03-10 17:31:12 +0100

    Switched to PNPM

--- src/app/services/mapbox.service.ts
+++ src/app/services/mapbox.service.ts
@@ -96,7 +96,6 @@ export class MapboxService {
 
   refreshMarkers(): void {
     this.markers.forEach((marker: Marker) => {
-      console.log(marker, this.uid)
       if (marker.id !== this.uid && (this.map.getZoom() < environment.mapbox.zoom || !this.markerVisible(marker))) {
         marker.marker.getElement().style.display = 'none';
       } else {
