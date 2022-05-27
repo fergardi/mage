062e70b222a9c0e19341a3509cbcc79c9643bb9a
Fergardi <fergardi@gmail.com>
2022-05-27 09:20:53 +0200

    Improved sidenav navigation with accordion

--- src/app/shared/shell/shell.component.ts
+++ src/app/shared/shell/shell.component.ts
@@ -26,31 +26,32 @@ import { MatAccordion } from '@angular/material/expansion';
 export class ShellComponent implements OnInit {
 
   public reports: number = 0;
+  public expanded: string = null;
   langs: any[] = [
     { lang: 'es', image: '/assets/images/languages/es.png' },
     { lang: 'en', image: '/assets/images/languages/en.png' },
     { lang: 'fr', image: '/assets/images/languages/fr.png' },
   ];
   groups: any[] = [
-    { name: 'shell.group.merchant', image: '/assets/images/cards/merchant.png', links: [
+    { id: 'merchant', name: 'shell.group.merchant', image: '/assets/images/cards/merchant.png', links: [
         { url: '/kingdom/city', name: 'kingdom.city.name', description: 'kingdom.city.description', image: '/assets/images/cards/city.png' },
         { url: '/kingdom/auction', name: 'kingdom.auction.name', description: 'kingdom.auction.description', image: '/assets/images/cards/auction.png' },
         { url: '/kingdom/emporium', name: 'kingdom.emporium.name', description: 'kingdom.emporium.description', image: '/assets/images/cards/emporium.png' },
       ],
     },
-    { name: 'shell.group.militia', image: '/assets/images/cards/militia.png', links: [
+    { id: 'militia', name: 'shell.group.militia', image: '/assets/images/cards/militia.png', links: [
         { url: '/world/map', name: 'world.map.name', description: 'world.map.description', image: '/assets/images/cards/map.png' },
         { url: '/kingdom/army', name: 'kingdom.army.name', description: 'kingdom.army.description', image: '/assets/images/cards/army.png' },
         { url: '/kingdom/tavern', name: 'kingdom.tavern.name', description: 'kingdom.tavern.description', image: '/assets/images/cards/tavern.png' },
       ],
     },
-    { name: 'shell.group.spy', image: '/assets/images/cards/spy.png', links: [
+    { id: 'spy', name: 'shell.group.spy', image: '/assets/images/cards/spy.png', links: [
         { url: '/kingdom/census', name: 'kingdom.census.name', description: 'kingdom.census.description', image: '/assets/images/cards/census.png' },
         { url: '/kingdom/archive', name: 'kingdom.archive.name', description: 'kingdom.archive.description', image: '/assets/images/cards/archive.png' },
         { url: '/kingdom/clan', name: 'kingdom.clan.name', description: 'kingdom.clan.description', image: '/assets/images/cards/clan.png' },
       ],
     },
-    { name: 'shell.group.scholar', image: '/assets/images/cards/scholar.png', links: [
+    { id: 'scholar', name: 'shell.group.scholar', image: '/assets/images/cards/scholar.png', links: [
         { url: '/kingdom/temple', name: 'kingdom.temple.name', description: 'kingdom.temple.description', image: '/assets/images/cards/temple.png' },
         { url: '/kingdom/sorcery', name: 'kingdom.sorcery.name', description: 'kingdom.sorcery.description', image: '/assets/images/cards/sorcery.png' },
         { url: '/user/encyclopedia', name: 'user.encyclopedia.name', description: 'user.encyclopedia.description', image: '/assets/images/cards/encyclopedia.png' },
@@ -98,7 +99,8 @@ export class ShellComponent implements OnInit {
     // accordion nav
     this.router.events.subscribe((event: RouterEvent) => {
       if (event instanceof NavigationEnd && event.url !== '/user/landing') {
-        // TODO: accordion index
+        const group = this.groups.find((g: any) => g.links.find((l: any) => event.url.includes(l.url)));
+        this.expanded = group ? group.id : null;
       }
     });
     // alerts
